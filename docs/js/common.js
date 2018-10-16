439ca0e2f3661dd80be99db2ebbf95860c19cb86
honeymaro <honeymaro@live.co.kr>
2018-10-16 17:33:28 +0900

    프로토타입을 제작한다

---
+++ docs/js/common.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    var windowHeight = $(window).height();
+    $(window).on("resize", function (e) {
+        windowHeight = $(window).height();
+        $(".fullHeight").css("min-height", windowHeight);
+        $(".halfHeight").css("min-height", windowHeight * 3 / 4);
+    });
+    $(window).resize();
+
+    $(window).scroll(function (e) {
+        var currentScroll = $(window).scrollTop();
+        if (currentScroll > 10) {
+            $(".header").addClass("scrolled");
+        }
+        else {
+            $(".header").removeClass("scrolled");
+        }
+    });
+
+});
