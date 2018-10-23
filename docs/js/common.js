07aee2173a87009b4255b2979a5420fd1dec9fcb
honeymaro <honeymaro@live.co.kr>
2018-10-23 13:38:56 +0700

    섹션 내부의 크기가 너무 클 경우 섹선 사이즈를 조정한다

--- docs/js/common.js
+++ docs/js/common.js
@@ -4,6 +4,13 @@ $(document).ready(function () {
         windowHeight = $(window).height();
         $(".fullHeight").css("min-height", windowHeight);
         $(".halfHeight").css("min-height", windowHeight * 3 / 4);
+
+        var contentCenter = $(".content-center");
+        for (i = 0; i < contentCenter.length; i++) {
+            if ($($(contentCenter[i]).find("div")[0]).height() * 1.2 > windowHeight) {
+                $(contentCenter[i]).css("min-height", $(contentCenter[i]).height() * 1.2);
+            }
+        }
     });
     $(window).resize();
 
@@ -17,7 +24,7 @@ $(document).ready(function () {
         }
     });
 
-    $("body").on("click", ".header-more", function(e){
+    $("body").on("click", ".header-more", function (e) {
         $(".header-menu").toggleClass("show");
     });
 });
