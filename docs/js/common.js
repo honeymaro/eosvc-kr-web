ef7151eb2bc71ae6d43f325efa5fc7bfeb53ee8a
honeymaro <honeymaro@live.co.kr>
2018-10-26 00:36:36 +0700

    텍스트 및 요소 사이즈 일부를 수정한다

--- docs/js/common.js
+++ docs/js/common.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
     $(window).on("resize", function (e) {
         windowHeight = $(window).height();
         $(".fullHeight").css("min-height", windowHeight);
-        $(".halfHeight").css("min-height", windowHeight * 3 / 4);
+        $(".halfHeight").css("min-height", windowHeight / 2);
 
         var contentCenter = $(".content-center");
         for (i = 0; i < contentCenter.length; i++) {
-            if ($($(contentCenter[i]).find("div")[0]).height() * 1.2 > windowHeight) {
-                $(contentCenter[i]).css("min-height", $(contentCenter[i]).height() * 1.2);
+            if ($($(contentCenter[i]).find("div")[0]).height() >= $(contentCenter[i]).height()) {
+                $(contentCenter[i]).css("min-height", $(contentCenter[i]).height() + 200);
             }
         }
     });
