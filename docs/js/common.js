b8e5862089e59fac266e3311edf45532cc7148a0
honeymaro <honeymaro@live.co.kr>
2018-10-29 14:04:16 +0900

    디자인을 약간 수정한다
    - 상단 배경화면 사이즈를 줄인다
    - 폰트 사이즈를 전체적으로 키운다
    - About 섹션의 내용 영역 사이즈를 조정한다

--- docs/js/common.js
+++ docs/js/common.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $(window).on("resize", function (e) {
         windowHeight = $(window).height();
         $(".fullHeight").css("min-height", windowHeight);
-        $(".halfHeight").css("min-height", windowHeight / 2);
+        $(".halfHeight").css("min-height", windowHeight / 5 * 2);
 
         var contentCenter = $(".content-center");
         for (i = 0; i < contentCenter.length; i++) {
